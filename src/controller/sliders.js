2a53ee2b13544974c4e6692570724a574a2c9d2a
Kahraman <80291704+kahramanX@users.noreply.github.com>
2023-02-06 04:51:09 +0300

    çok satanlar slider added and designed

---
+++ src/controller/sliders.js
@@ -0,0 +1,52 @@
+let Swiper = require("swiper/swiper-bundle");
+
+// Main Slider
+let mainSlider = new Swiper(".MainSlider", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  loop: true,
+  centeredSlides: true,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// BestSeller Slider
+let bestSellerSlider = new Swiper(".BestsellerSlider", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 2,
+  spaceBetween: 20,
+  loop: true,
+  centeredSlides: true,
+
+  breakpoints: {
+    1440: {
+      slidesPerView: 5,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require("./assets/styles/widgets.scss");
 require("./assets/styles/index.scss");
 require("./assets/styles/header.scss");
 require("./assets/styles/mainSlider.scss");
+require("./assets/styles/bestSellerSlider.scss");
 require("./assets/styles/variables.scss");
 
 console.log("index.js file");
